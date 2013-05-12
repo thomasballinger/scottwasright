a46f583596a080beab017e31a22db327fffd4fb4
Thomas Ballinger <thomasballinger@gmail.com>
2013-05-12 07:19:50 -0700

    factor out paint, start work on window size change

--- simple.py
+++ simple.py
@@ -8,6 +8,12 @@
 * maybe scroll down all the way immediately? Have an option for that maybe?
 
 * good deal of redraw optimizations to be made - nothing should disappear
+
+* Vertial window size change: always try to scroll down for each move down
+  * these scrolls shouldn't do anything if history is at top of window
+  * on vertical smaller, scroll down iff we need the space
+  * actually, on scroll up, perhaps th eright thing will just happen?
+  * eventually catch the change window events, for now just compare size between paints
 """
 
 
@@ -15,30 +21,36 @@ import sys
 import os
 import re
 import tty
+import functools
+import signal
 
-def up(n=1):
-    if n: sys.stdout.write("[%dA" % n)
-def down(n=1):
-    if n: sys.stdout.write("[%dB" % n)
-def fwd(n=1):
-    if n: sys.stdout.write("[%dC" % n)
-def back(n=1):
-    if n: sys.stdout.write("[%dD" % n)
-def erase_rest_of_line(): sys.stdout.write("[K")
+
 
 QUERY_CURSOR_POSITION = "\x1b[6n"
 
+def move_cursor_direction(char, n=1):
+    if n: sys.stdout.write("[%d%s" % (n, char))
+up, down, fwd, back = [functools.partial(move_cursor_direction, char) for char in 'ABCD']
+
+def erase_rest_of_line(): sys.stdout.write("[K")
+
 class TerminalWrapper(object):
-    """The model here is of a much larger screen, so ideally we can ignore scrolling"""
+    """The model here is of a much larger screen, so scrolling can be ignored
+    by the user of this api"""
 
     def __init__(self):
         self.current_line = ''
         self.stdin_buffer = []
         self.scroll_offset = 0
         self.logical_lines = []
+
+        # lines separated whenever logical line length goes over
+        #   what the terminal width was at the time of original output
         self.display_lines = []
+
         self.initial_screen_row = None
         self.pos = [0, 1]
+        self.last_key_pressed = None
 
     def get_char(self):
         """Use this in case a query was issued and input
@@ -138,11 +150,15 @@ class TerminalWrapper(object):
         pass
 
     def process_char(self, char):
+        self.last_key_pressed = char
         if char == '':
             self.pos[1] = max(self.pos[1] - 1, 1)
             self.current_line = self.current_line[:-1]
         elif char == "":
             raise KeyboardInterrupt()
+        elif char == "":
+            os.system('reset')
+            sys.exit()
         elif char == """""" or char == "\n" or char == "\r": # return key, processed, or ?
             self.logical_lines.append(self.current_line)
             self.display_lines.append(self.current_line) #TODO proper display line handling
@@ -151,33 +167,42 @@ class TerminalWrapper(object):
             self.pos[1] = 1
             if self.pos[0] + self.initial_screen_row - self.scroll_offset > self.get_screen_size()[0]:
                 self.scroll_down()
+        elif char == "" or char == "":
+            pass #dunno what these are, but they screw things up
         else:
             self.current_line += char
             self.pos[1] += 1 #TODO handle wrapping
         #TODO deal with characters that take up more than one space
 
+    def paint(self):
+        self.set_screen_pos((self.pos[0] + self.initial_screen_row - self.scroll_offset, self.pos[1]))
+        top_line_we_own = self.initial_screen_row - self.scroll_offset
+        for i, line in zip(range(top_line_we_own, top_line_we_own + len(self.display_lines)), self.display_lines):
+            if i > 0:
+                self.set_screen_pos((i, 0))
+                sys.stdout.write(line)
+                erase_rest_of_line()
+        back(1000)
+        for i in range(100):
+            down()
+            erase_rest_of_line()
+        self.set_screen_pos((self.pos[0] + self.initial_screen_row - self.scroll_offset, 0))
+        sys.stdout.write(self.current_line)
+        erase_rest_of_line()
+        self.info_screen(repr(self))
+        fwd(len(self.current_line))
+
     def _run(self):
         tty.setraw(sys.stdin)
         self.initial_screen_row, _ = self.get_screen_position()
+        signal.signal(signal.SIGWINCH, lambda signum, frame: self.window_change_event())
         while True:
-            self.set_screen_pos((self.pos[0] + self.initial_screen_row - self.scroll_offset, self.pos[1]))
+            self.paint()
             c = self.get_char()
             self.process_char(c)
-            top_line_we_own = self.initial_screen_row - self.scroll_offset
-            for i, line in zip(range(top_line_we_own, top_line_we_own + len(self.display_lines)), self.display_lines):
-                if i > 0:
-                    self.set_screen_pos((i, 0))
-                    sys.stdout.write(line)
-                    erase_rest_of_line()
-            back(1000)
-            for i in range(100):
-                down()
-                erase_rest_of_line()
-            self.set_screen_pos((self.pos[0] + self.initial_screen_row - self.scroll_offset, 0))
-            sys.stdout.write(self.current_line)
-            erase_rest_of_line()
-            self.info_screen(repr(self))
-            fwd(len(self.current_line))
+
+    def window_change_event(self):
+        print 'window changed!'
 
     def __repr__(self):
         s = ''
@@ -185,6 +210,8 @@ class TerminalWrapper(object):
         s += " rows above/below:" + repr(self.rows_above_below()) + '\n'
         s += " cursor_pos:" + repr(self.get_screen_position()) + '\n'
         s += " pos:" + repr(self.pos) + '\n'
+        s += " last key presed:" + repr(self.last_key_pressed) + '\n'
+        s += " lines scrolled down:" + repr(self.scroll_offset) + '\n'
         s += '>'
         return s
 
