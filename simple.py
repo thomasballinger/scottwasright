31dd3ba349bcd198af3fd6cec6595f8b103367d4
Thomas Ballinger <thomasballinger@gmail.com>
2013-05-20 17:28:30 -0400

    start trimming Repl

--- simple.py
+++ simple.py
@@ -1,108 +1,54 @@
 """
 
-* Let's do our own line wrapping, so we can remember how the line was wrapped
-    so when we replace it under an informational window, we have enough
-    information to do so
-* what to do about scrolling up? On keypress, scroll all the way back down?
-
 * maybe scroll down all the way immediately? Have an option for that maybe?
 
-* good deal of redraw optimizations to be made - nothing should disappear
-
-* Vertial window size change: always try to scroll down for each move down
-  * these scrolls shouldn't do anything if history is at top of window
-  * on vertical smaller, scroll down iff we need the space
-  * actually, on scroll up, perhaps th eright thing will just happen?
-  * eventually catch the change window events, for now just compare size between paints
-
-
-Terminals never scroll up by themselves, right?
 """
 
 
 import sys
 import os
-import re
 import tty
-import functools
 import signal
 
+class Repl(object):
+    """
 
+    Renders 2d arrays of characters
 
-QUERY_CURSOR_POSITION = "\x1b[6n"
-
-def move_cursor_direction(char, n=1):
-    if n: sys.stdout.write("[%d%s" % (n, char))
-up, down, fwd, back = [functools.partial(move_cursor_direction, char) for char in 'ABCD']
-
-def erase_rest_of_line(): sys.stdout.write("[K")
-def erase_line(): sys.stdout.write("[2K")
-
-class TerminalWrapper(object):
-    """The model here is of a much larger screen, so scrolling can be ignored
-    by the user of this api"""
+    takes in:
+     -terminal dimensions and change events
+     -keystrokes
+     -scroll down events
+     -initial cursor position
+"""
 
     def __init__(self):
-        self.stdin_buffer = []
-
         self.current_line = ''
         self.logical_lines = []
         self.display_lines = [] # lines separated whenever logical line
                                 # length goes over what the terminal width
                                 # was at the time of original output
+
+        self.initial_screen_row = None
         self.scroll_offset = 0
         self.cursor_offset_in_line = 0
-        self.info_msg = '<filler info msg>'
         self.last_key_pressed = None
-        self.initial_screen_row = None # initialized in _run
+
+    def dumb_paint(self):
+        for line in self.display_lines:
+            print line
+        for line in self.display_linize(self.current_line):
+            print line
+
+    def dumb_input(self):
+        for c in raw_input('>'):
+            if c in '/':
+                c = '\n'
+            self.process_char(c)
 
     @property
     def current_line_start_screen_row(self):
         """Should only be called when cursor is at a resting place"""
-
-
-    def get_char(self):
-        """Use this in case a query was issued and input
-        had to be read in to answer it, but another character or so
-        was read in at that time as well"""
-        if self.stdin_buffer:
-            return self.stdin_buffer.pop(0)
-        else:
-            return sys.stdin.read(1)
-
-    def get_screen_position(self):
-        """Returns the terminal (row, column) of the cursor"""
-        sys.stdout.write(QUERY_CURSOR_POSITION)
-        resp = ''
-        while True:
-            c = sys.stdin.read(1)
-            resp += c
-            m = re.search('(?P<extra>.*)\x1b\[(?P<row>\\d+);(?P<column>\\d+)R', resp)
-            if m:
-                row = int(m.groupdict()['row'])
-                col = int(m.groupdict()['column'])
-                self.stdin_buffer.extend(list(m.groupdict()['extra']))
-                return (row, col)
-
-    def set_screen_pos(self, (row, col)):
-        sys.stdout.write("[%d;%dH" % (row, col))
-
-    def scroll_down(self):
-        sys.stderr.write("D")
-        self.scroll_offset += 1
-
-    def get_screen_size(self):
-        orig = self.get_screen_position()
-        fwd(10000)
-        down(10000)
-        size = self.get_screen_position()
-        self.set_screen_pos(orig)
-        #return size
-        return (size[0], 13)
-
-    def info_screen(self):
-        """msg should not have lines longer than current width of screen"""
-        #TODO implement a max height these things can be
 
     def rewrite_our_lines(self):
         """Rewrites saved lines to screen as they were before"""
@@ -139,7 +85,7 @@ class TerminalWrapper(object):
         #TODO deal with characters that take up more than one space
 
     def display_linize(self, msg):
-        rows, columns = self.get_screen_size()
+        columns = self.columns
         display_lines = ([self.current_line[start:end]
                     for start, end in zip(
                         range(0, len(self.current_line), columns),
@@ -162,13 +108,6 @@ class TerminalWrapper(object):
         output_lines.append('+'+'-'*width+'+')
         return output_lines
 
-    def force_down(self):
-        total_rows, _ = self.get_screen_size()
-        current_row, _ = self.get_screen_position()
-        if current_row == total_rows:
-            self.scroll_down()
-        down()
-
     def paint_history(self):
         rows, columns = self.get_screen_size()
         first_screen_row_to_render = max(1, self.initial_screen_row - self.scroll_offset)
@@ -177,7 +116,6 @@ class TerminalWrapper(object):
         while 0 <= display_line_for_screen_line(screen_row) < len(self.display_lines):
             self.set_screen_pos((screen_row, 1))
             sys.stdout.write(self.display_lines[display_line_for_screen_line(screen_row)][:columns])
-            erase_rest_of_line()
             screen_row += 1
 
     def paint_current_line(self):
@@ -246,8 +184,6 @@ class TerminalWrapper(object):
         #TODO Don't repaint everything every time! Don't even repaint the current line if not necessary
         self.info_msg = repr(self)
 
-        self.
-
         self.paint_history()
         #self.paint_current_line()
         self.paint_infobox()
@@ -284,4 +220,8 @@ class TerminalWrapper(object):
             os.system('reset')
 
 if __name__ == '__main__':
-    TerminalWrapper().run()
+    r = Repl()
+    while True:
+        r.columns = 14
+        r.dumb_paint()
+        r.dumb_input()
