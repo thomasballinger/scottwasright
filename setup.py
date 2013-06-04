b78b6cc0e8618e4c58af31bdcad8abdeaf1d04ba
Thomas Ballinger <thomasballinger@gmail.com>
2013-06-04 17:21:43 -0400

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scottsright',
+      version='0.0.1',
+      description='library for line-oriented, interactive text-based user interfaces',
+      url='https://github.com/thomasballinger/scottwasright',
+      author='Thomas Ballinger',
+      author_email='thomasballinger@gmail.com',
+      license='MIT',
+      packages=['scottsright'],
+      zip_safe=False)
