60047b325117d998946c9509327066abea22c4df
Thomas Ballinger <thomasballinger@gmail.com>
2013-06-21 16:05:59 -0400

    implement or stub out all simple readline methods

--- scottsright/manual_readline.py
+++ scottsright/manual_readline.py
@@ -38,6 +38,42 @@ def backspace(cursor_offset, line):
     return (max(cursor_offset - 1, 0),
             line[:max(0, cursor_offset-1)] + line[cursor_offset:])
 
+@on('[3~')
+def delete(cursor_offset, line):
+    return (cursor_offset,
+            line[:cursor_offset] + line[cursor_offset+1:])
+
+@on('')
+def delete_from_cursor_back(cursor_offset, line):
+    return 0, line[cursor_offset:]
+
+@on('')
+def delete_from_cursor_forward(cursor_offset, line):
+    return cursor_offset, line[:cursor_offset]
+
+@on('d')
+def delete_rest_of_word(cursor_offset, line):
+    raise NotImplementedError()
+
+@on('')
+def delete_word_to_cursor(cursor_offset, line):
+    raise NotImplementedError()
+
+@on('')
+def yank_prev_killed_text(cursor_offset, line):
+    raise NotImplementedError()
+
+@on('y')
+def yank_prev_prev_killed_text(cursor_offset, line):
+    raise NotImplementedError()
+
+@on('')
+def transpose_character_before_cursor(cursor_offset, line):
+    raise NotImplementedError()
+
+@on('t')
+def transpose_word_before_cursor(cursor_offset, line):
+    raise NotImplementedError()
 
 if __name__ == '__main__':
     from pprint import pprint
